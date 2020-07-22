569dde224aeef5dc722e4a65118c0dd140a51658
Guillaume Viger <fenekku@fenekku.com>
2020-07-22 14:09:32 -0500

    tests: use pytest directly [more]
    See https://github.com/inveniosoftware/invenio-app-rdm/issues/191

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University.
+# Copyright (C) 2019-2020 CERN.
+# Copyright (C) 2019-2020 Northwestern University.
 #
 # Invenio-Records-Permissions is free software; you can redistribute it
 # and/or modify it under the terms of the MIT License; see LICENSE file for
@@ -12,4 +12,4 @@ pydocstyle invenio_records_permissions tests docs && \
 isort invenio_records_permissions tests --check-only --diff && \
 check-manifest --ignore ".travis-*,.vscode*" && \
 sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test
+pytest
