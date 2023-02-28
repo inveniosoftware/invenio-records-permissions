e5b6094fe03aea272f7fca03529c55affb13e49b
David Eckhard <dabombeeer@gmail.com>
2023-02-28 14:51:08 +0100

    transifex: update config
    ci: use i18n reusable workflows

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2019-2020 CERN.
 # Copyright (C) 2019-2020 Northwestern University.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # Invenio-Records-Permissions is free software; you can redistribute it
 # and/or modify it under the terms of the MIT License; see LICENSE file for
@@ -24,7 +24,7 @@ trap cleanup EXIT
 
 
 python -m check_manifest
-python -m setup extract_messages --dry-run
+python -m setup extract_messages --output-file /dev/null
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --cache ${CACHE:-redis} --env)"
 python -m pytest
